ff7e3c9afc96251c3237363115ef3786e2a500af
depanirenish <106726605+depanirenish@users.noreply.github.com>
2023-09-27 18:22:19 +0530

    Delete wp-includes directory

--- wp-includes/blocks/navigation/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'efa7b39e8a2ac7d3f83a');
--- wp-includes/blocks/navigation/view.min.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'dde7635cad8b0819b900');
